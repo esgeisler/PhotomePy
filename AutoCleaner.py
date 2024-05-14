e3d9db32c8b137f8970c0cbc32bbedaaabb2dd25
esgeisler <geisleres@vcu.edu>
2024-05-14 16:07:26 -0400

    Implemented baselineSubtractor (needs fixing )

--- AutoCleaner.py
+++ AutoCleaner.py
@@ -48,6 +48,24 @@ def RBaselineGet(FileName):
 
     return mean470, mean405
 
+#TODO Function that subtracts baseline from baseline getters from a chosen channel, returns those as full channel trace dictionaries
+def baselineSubtractor(fileName, baselines, channelToSubtract1, channelToSubtract2):
+    abf = pyabf.ABF(fileName)
+    sweepDict470 = {}
+    sweepDict405 = {}
+    if type(baselines) is not list:
+        raise TypeError("Values for baseline not a list.")
+    #470
+    abf.setSweep(sweepNumber= 0, channel= channelToSubtract1)
+    for sweeps in abf.sweepList:
+        sweepDict470[sweeps] = abf.sweepY - baselines[0]
+    #405
+    abf.setSweep(sweepNumber= 0, channel= channelToSubtract2)
+    for sweeps in abf.sweepList:
+        sweepDict405[sweeps] = abf.sweepY - baselines[1]
+    subtractedSweeps = [sweepDict470, sweepDict470]
+    return subtractedSweeps
+
 # Gaussian filters a single trace with a 40 Hz cutoff frequency, based on pClamp documentation and Calquhon & Sigworth (1995)
 def gaussianFilter(FileName, filterChannel, filterSweep):
     abf = pyabf.ABF(FileName)
@@ -55,6 +73,7 @@ def gaussianFilter(FileName, filterChannel, filterSweep):
     sweepList = scipy.ndimage.gaussian_filter1d(abf.sweepY, sigma= 22)
     return sweepList
 
+# Gaussian filters an entire channel with a 40 Hz cutoff freq., as above.
 def wholeTraceGauss(fileName, filterChannel):
     abf = pyabf.ABF(fileName)
     sweepDict = {}
@@ -64,3 +83,7 @@ def wholeTraceGauss(fileName, filterChannel):
         filteredSweep = scipy.ndimage.gaussian_filter1d(abf.sweepY, sigma = 16)
         sweepDict[sweeps] = filteredSweep
     return sweepDict
+
+#TODO Divides two channels in one file and returns a complete channel dictionary.
+def ratio470405(fileName, channel470, channel405):
+    return
