b441b96db7463d48c3f5d30dfca505c21ad0eee8
esgeisler <geisleres@vcu.edu>
2024-05-20 11:23:06 -0400

    Implemented deltaF and bleachingSub

--- AverageTraces.py
+++ AverageTraces.py
@@ -1,5 +1,6 @@
 import statistics as stat
 
+# Averages every sweep's voltage (fluorescence) in a trace, producing a dictionary of 70 averages
 def traceAverage(processedSignal):
     meanSignal = {}
     x = 0
@@ -8,9 +9,27 @@ def traceAverage(processedSignal):
         x += 1
     return meanSignal
 
+# Averages the sweeps before a certain time point to create a "Pre-Injection Average" of fluorescence
 def preInjectionAverage(averagedSignal, injectionTrace):
     preInjList = []
     for sweep in averagedSignal[:injectionTrace]:
         preInjList.append(sweep)
     preInjAvg = stat.mean(preInjList)
     return preInjAvg
+
+# Calculates ΔF/F ((trace avg - pre inj avg)/pre inj avg)
+def deltaF(averagedSignal, preInjAvg):
+    deltaFDivided = {}
+    for sweep in averagedSignal:
+        deltaFDivided[sweep] = (averagedSignal[sweep] - preInjAvg)/preInjAvg
+    return deltaFDivided
+
+# Bleaching correction that returns difference between ΔF/F (drug) and ΔF/F (vehicle)
+def bleachingSub(signalDrug, signalVehicle):
+    finalSignal = {}
+    x = 0
+    dictLen = len(signalDrug)
+    for x in dictLen:
+        finalSignal[x] = signalDrug[x] - signalVehicle[x]
+        x += 1
+    return finalSignal
