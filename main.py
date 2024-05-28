fa5a6007b3fb796293d1616a50d530a482079198
esgeisler <geisleres@vcu.edu>
2024-05-28 10:16:38 -0400

    Implement fileBrowser

--- main.py
+++ main.py
@@ -7,35 +7,49 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 
-def fileBrowser():
-    fileName = filedialog.askopenfilename(initialdir= "/", title= "Select a Main File", filetypes=(("Axon Binary Fles", "*.abf*"), ("All Files," "*.*")))
+
 
 class Main(tk.Frame):
     def __init__(self, master= None, **kwargs):
         super().__init__(master, **kwargs)
 
+        self.experimentFileName = ""
+        self.baselinefileName = ""
+
+        def fileBrowserExperiment():
+            self.experimentFileName = filedialog.askopenfilename(initialdir= "/", title= "Select a Main File", filetypes=(("Axon Binary Fles", "*.abf*"), ("All Files," "*.*")))
+            print(self.experimentFileName)
+            chosenFileDisplay.insert(tk.END, self.experimentFileName)
+            return self.experimentFileName
+        
+        def fileBrowserBaseline():
+            self.baselinefileName = filedialog.askopenfilename(initialdir= "/", title= "Select a Main File", filetypes=(("Axon Binary Fles", "*.abf*"), ("All Files," "*.*")))
+            print(self.baselinefileName)
+            baselineFileDisplay.insert(tk.END, self.baselinefileName)
+            return self.baselinefileName
+        
         decisionLabel = tk.Label(self, text="File Process")
         userChosenProcess = ttk.Combobox(self, state= "readonly", values=["Get Baseline", "Get Averages"])
         userChosenProcess.set("Get Baseline")
         decisionLabel.pack()
         userChosenProcess.pack()     
 
-        explorerButton = tk.Button(self, text="Choose a Main File", command= fileBrowser)
-        baselineExplorerButton = tk.Button(self, text="Choose a Baseline File", command= fileBrowser)
+        explorerButton = tk.Button(self, text="Choose a Main File", command= fileBrowserExperiment)
+        chosenFileDisplay = tk.Text(self, height= 1, width= 52)
+        chosenFileDisplay.pack()
+        baselineExplorerButton = tk.Button(self, text="Choose a Baseline File", command= fileBrowserBaseline)
         explorerButton.pack()
+        baselineFileDisplay = tk.Text(self, height= 1, width= 52)
+        baselineFileDisplay.pack()
         baselineExplorerButton.pack()
-    
-    
+        runFileButton = tk.Button(self, text="Run Program")
+        runFileButton.pack()
 
-# runFileButton = tk.Button(fp, text="Run Program")
-# explorerButton.grid(column= 1, row= 2)
-# baselineExplorerButton.grid(column= 2, row= 2)
-# runFileButton.grid(column=2, row= 3)
-
+        chosenFileDisplay.insert(tk.END, self.experimentFileName)
+        baselineFileDisplay.insert(tk.END, self.baselinefileName)
 
 # abf = pyabf.ABF(userFile)
-# if dataType == "raw":
-#     baselineFile = fls.FileSelect("raw", input("What is the name of the file to be used for baseline?"))
+# baselineFile = fls.FileSelect("raw", input("What is the name of the file to be used for baseline?"))
 # decision =  input("What shall we do today?")
 
 # if decision == "baseline":
