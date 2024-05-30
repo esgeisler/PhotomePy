7764939a920a014670635de6db16054a5fd1fabe
esgeisler <geisleres@vcu.edu>
2024-05-30 12:47:07 -0400

    peakAnalysis.py

---
+++ peakAnalysis.py
@@ -0,0 +1,24 @@
+import AutoCleaner as acl
+import AverageTraces as avg
+import scipy as sci
+import pyabf
+import matplotlib.pyplot as plt
+
+#TODO: Write a function that retrieves the number of peaks
+def peakGetter():
+    return
+
+#TODO: Write a house-keeping function that displays those peaks on a graph
+
+
+
+
+# Plots the original, subtracted, ratio-ed, and processed trace of choice
+    # abf.setSweep(sweepNumber= userTrace, channel= userChannel)
+    # plt.plot(abf.sweepX[1000:-2000], abf.sweepY[1000:-2000], color="b", label="Original")
+    # plt.plot(abf.sweepX[1000:-2000], subtractRight[0][userTrace][1000:-2000], color="r", label= "Subtracted")
+    # plt.plot(abf.sweepX[1000:-2000], ratioSignalRight[userTrace][1000:-2000], color="y", label= "Ratio-ed")
+    # plt.plot(abf.sweepX[1000:-2000], finalSignalRight[userTrace][1000:-2000], color="g", label= "Processed")
+    # plt.axis([0,15,-1,6])
+    # plt.legend()
+    # plt.show()
