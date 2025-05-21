63c65f5638be25789f35d8377aeeb9335fb4b30f
esgeisler <geisleres@vcu.edu>
2025-05-21 11:23:38 -0400

    Moved some variables to totalPeaks

--- processedSignal.py
+++ processedSignal.py
@@ -17,16 +17,7 @@ class ProcessedTotalSignal():
         self.traceLength = len(self.processedSignalArray[0])
         self.date = self.abf.abfDateTime.strftime("%Y-%m-%d")
 
-        self.samplingFreqMSec = self.abf.dataPointsPerMs + (1/3)
-        self.samplingFreqSec = self.samplingFreqMSec * 1000
-        self.seconds = np.arange(0, 47750, self.samplingFreqSec)
-
         self.wholeTraceAverages, self.wholeTraceMedians = avg.traceAverage(self.processedSignalArray)
         self.preInjAvg, self.preInjStdev = avg.preInjectionAverage(self.processedSignalArray, self.injTrace)
         self.normFluorescence = avg.deltaF(self.wholeTraceAverages, self.preInjAvg)
         self.overallZScore = avg.zCalc(self.wholeTraceAverages, self.processedSignalArray, self.injTrace)
-
-    #     self.mostPeaksInTrace = pas.peakMax(self.processedSignalArray)
-    #     self.peaksArray, self.peaksDict, self.widthBottomArray, self.width10Array, self.widthHalfArray, self.width90Array = pas.peakFinder(self.processedSignalArray, self.experimentFile)
-
-    
